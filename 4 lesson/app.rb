5fb72776a3a8c9158003759d4088bf4b5728fcf5
pradoav <a.rozputnii@gmail.com>
2019-11-04 22:32:10 +0200

    add bootstrap && css

--- 4 lesson/app.rb
+++ 4 lesson/app.rb
@@ -4,24 +4,61 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'yaml'
 require_relative 'tamagotchi.rb'
+$pets = Pets.new
+
 
 get '/' do
 	erb :index
 end
 
+post '/' do
+	if params[:namegame]
+		erb :change_pet
+	elsif params[:continuegame]
+		redirect :'/continuegame'
+	end
+end	
+
 get '/change_pet' do
-	   
 	erb :change_pet
 end
-post '/change_pet' do
-	@name_pets = params[:name_pets]
-	$pets = Pets.new
 
+post '/change_pet' do
+	$name = params[:name_pets]
+	$hp = 100; $hunger = 50; $power = 50; $mood = 50
 	erb :game
 end
-post '/game' do
-		
 
+post '/game' do
+	game		
+end
+
+get '/continuegame' do
+	savegame = YAML.load(File.read("data.yml"))
+	$name 	= savegame[0]["name"]
+	$hp 		= savegame[0]["health"]	
+	$hunger = savegame[0]["hunger"]
+	$power 	= savegame[0]["power"]
+	$mood 	= savegame[0]["mood"]
+	erb :game
+end
+
+post '/continuegame' do
+	erb :game
+end
+
+
+def write_yaml
+				config = [
+				"name" => $name,
+				"health" => $hp, 
+				"hunger" => $hunger,
+				"power" => $power,
+				"mood" => $mood]
+			File.open("data.yml", "w") { |file| file.write(config.to_yaml) }			
+end
+
+def game
 		if params[:feed]
 			$pets.feed
 		elsif params[:play]
@@ -39,21 +76,14 @@ post '/game' do
 		elsif params[:fleas]
 			$pets.fleas
 		elsif params[:quit]
-			config = ["health" => "#{$hp}", 
-				"hunger" => "#{$hunger}",
-				"power" => "#{$power}",
-				"mood" => "#{$mood}"]
-				
-			File.open("data.yml", "w") { |file| file.write(config.to_yaml) }				
+			write_yaml
+			redirect :/
 		end
+
 		if $hp > 0	
 			erb :game
 		else		
-
-			"gameover"
+			write_yaml
+			erb :dead
 		end	
-		
-end
-
-
-
+end		
--- 4 lesson/tamagotchi.rb
+++ 4 lesson/tamagotchi.rb
@@ -2,11 +2,11 @@ class Pets
 
 
  def initialize
-   @name 
-   $hp = 100
-   $hunger = 50
-   $power = 50
-   $mood = 50
+   $name 
+   $hp 
+   $hunger 
+   $power 
+   $mood 
  end 
 
  # погодувать
@@ -15,7 +15,7 @@ class Pets
    $power += rand(5...10)
    $mood += rand(-20...20)
    $message = "Вы покормили своего малыша"
-   $message = "#{@name} рад что его кормили :)"
+   $message = "#{$name} рад что его кормили :)"
    change_hp
  end
  # играть
@@ -23,7 +23,7 @@ class Pets
    $hunger += rand(5...40)
    $mood -= rand(10...30)
    $power -= rand(10...20)
-   $message = "Вы одели на #{@name} намордник и пошли гулять в парк. "
+   $message = "Вы одели на #{$name} намордник и пошли гулять в парк. "
    change_hp
  end
  # вкласти спати
@@ -32,7 +32,7 @@ class Pets
    $power += rand(20...60)
    $hunger += rand(20...50)
    change_hp
-   $message = "#{@name} пошёл спать"
+   $message = "#{$name} пошёл спать"
    
  end
  # прогулятися
@@ -50,20 +50,20 @@ class Pets
    r = rand(1..4)
    case r
      when 1
-     $message = "#{@name} начал играться и погрыз диван, вам пришлось у воспитательных целять показать кто в доме man"
+     $message = "#{$name} начал играться и погрыз диван, вам пришлось у воспитательных целять показать кто в доме man"
      $hp -= rand(5...30)
      $hunger += rand(5...10)
      $mood -= rand(0...20)
      $power -= rand(10...20)
      change_hp
      when 2
-     $message = "#{@name} притащил в зубах тапочки к постели"
+     $message = "#{$name} притащил в зубах тапочки к постели"
      $hunger += rand(5...10)
      $mood += rand(10...20)
      $power -= rand(0...10)
      change_hp
      when 3
-     $message = "#{@name} вырвался на улицу и встретил shemale. Вернулся грязный но очень доволен :)"
+     $message = "#{$name} вырвался на улицу и встретил shemale. Вернулся грязный но очень доволен :)"
      $hunger += rand(5...10)
      $mood += rand(40...80)
      $power -= rand(5...40)
@@ -87,7 +87,7 @@ class Pets
    change_hp
  end
   def training
-   $message = "#{@name} хорошо поддается дрессировки . Ему нравится что ты придаешь ему внимание. Не забудь покормить его."
+   $message = "#{$name} хорошо поддается дрессировки . Ему нравится что ты придаешь ему внимание. Не забудь покормить его."
    $hunger += rand(10...20)
    $mood += rand(20...35)
    $power += rand(10...25)
@@ -110,7 +110,7 @@ class Pets
    if $hunger.to_i > 100
    		$hunger = 100
      $hp -= rand(20...30)
-     $message = " #{@name} ну очень голоден покорми уже его , изверг"
+     $message = " #{$name} ну очень голоден покорми уже его , изверг"
    elsif $hunger.to_i < 0
 		$hunger = 0
      $hp += rand(0...5)
@@ -122,7 +122,7 @@ class Pets
    elsif $mood.to_i < 0
    	$mood = 0
     $hp -= rand(1...5)
-    $message = "#{@name} имеет плохое настроение "
+    $message = "#{$name} имеет плохое настроение "
     end
    if $power.to_i > 100
    	$power = 100
@@ -130,7 +130,7 @@ class Pets
    elsif $power.to_i < 0
    	$power = 0
     $hp -= rand(10...20) 
-    $message = "#{@name} чувствует себя очень хорошо"
+    $message = "#{$name} чувствует себя очень хорошо"
     end
    if $hp <= 0
      $message = "Gameover"
